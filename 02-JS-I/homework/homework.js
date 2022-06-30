62f291c12f3906f02f5eafc1aa0f11fe3d0f9f70
Lucas Heredia <heredialucas1@gmail.com>
2022-06-30 11:40:41 -0300

    cambio de numero

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = '9';
+const nuevoNum = '11'
 
 // Crea una variable booleana:
 const nuevoBool = null;
